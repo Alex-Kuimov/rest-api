48abdbf8e9312da662b0aff5815e5dd341ba4a2b
Alex Kuimov <spoot1986@gmail.com>
2023-05-15 12:14:34 +0300

    Fix access logic

--- app/Guard.php
+++ app/Guard.php
@@ -7,6 +7,7 @@ class Guard
     private string $authTable;
     private string $usersTable;
     private string $postsTable;
+    private string $accessTable;
     public ?int $userID;
 
     public function __construct()
@@ -14,78 +15,14 @@ class Guard
         $this->authTable = 'auth';
         $this->usersTable = 'users';
         $this->postsTable = 'posts';
+        $this->accessTable = 'access';
         $this->query = new Query;
     }
 
     public function auth():bool
     {
-        return $this->isAuth();
-    }
+        //return $this->isAuth();
 
-    public function access($route, $method, $postID): bool
-    {
-        $roles = ['admin', 'editor', 'subscriber'];
-        $user = $this->query->get($this->usersTable, 'all', ['id' => $this->userID]);
-        $role = $user[0]['role'];
-
-        if (!in_array($role, $roles, true)) {
-            return false;
-        }
-
-        if ($role === 'subscriber' && $method !== 'get') {
-            return false;
-        }
-
-        if ($this->deniedSystemEdit($role, $route)) {
-            return false;
-        }
-
-        if ($this->deniedPostEdit($role, $method, $postID)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private function deniedSystemEdit($role, $route): bool
-    {
-        $routes = ['options', 'users'];
-        $roles = ['editor', 'subscriber'];
-
-        if (in_array($role, $roles, true) && in_array($route, $routes, true)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function deniedPostEdit($role, $method, $itemID): bool
-    {
-        $roles = ['editor'];
-        $methods = ['get', 'create', 'update', 'delete'];
-
-        if (in_array($role, $roles, true) && in_array($method, $methods, true)) {
-            if ($method === 'get' || $method === 'create') {
-                return false;
-            }
-
-            if ($method === 'update' || $method === 'delete') {
-                $posts = $this->query->get($this->postsTable, 'all', ['id' => $itemID]);
-                $userID = $posts[0]['user_id'];
-
-                if ($this->userID === $userID) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private function isAuth():bool
-    {
         $headers = getallheaders();
         $headerToken = $headers['Auth'] ?? null;
 
@@ -100,5 +37,45 @@ class Guard
         $this->userID = !empty($auth) ? $auth[0]['user_id'] : null;
 
         return $token === $headerToken;
+    }
+
+    public function access($route, $method, $postID): bool
+    {
+        $permissions = $this->getPermission();
+        $actionAccess = $permissions[$method.'_action'];
+        $systemAccess = $permissions[$route.'_action'] ?? null;
+        $allAccess = $permissions['all_items'];
+
+        if (!$actionAccess) {
+            return false;
+        }
+
+        if (isset($systemAccess) && !$systemAccess) {
+            return false;
+        }
+
+        if (!$allAccess && ($method === 'update' || $method === 'delete') && !$this->isOwner($postID)) {
+            return false;
+        }
+
+        return $actionAccess;
+    }
+
+    private function getPermission()
+    {
+        $user = $this->query->get($this->usersTable, 'all', ['id' => $this->userID]);
+        $role = $user[0]['role'];
+
+        $access = $this->query->get($this->accessTable, 'all', ['role' => $role]);
+
+        return $access[0] ?? null;
+    }
+
+    private function isOwner($postID): bool
+    {
+        $posts = $this->query->get($this->postsTable, 'all', ['id' => $postID]);
+        $userID = $posts[0]['user_id'] ?? null;
+
+        return $this->userID === $userID;
     }
 }
